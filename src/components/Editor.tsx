933a2bb916db4d8ed8df4bb477e6d274442f0b82
David Silgado Salcedo <dsilgadosalcedo@gmail.com>
2024-08-05 10:21:53 -0500

    Refactor editor logo

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,7 @@
+import { emmetHTML, emmetCSS } from "emmet-monaco-es"
 import { useEffect, useRef } from "react"
 import * as monaco from "monaco-editor"
-import { emmetHTML, emmetCSS } from "emmet-monaco-es"
+import EditorLogo from "./EditorLogo"
 
 emmetHTML(monaco)
 emmetCSS(monaco)
@@ -48,41 +49,7 @@ function Editor({
 	return (
 		<div className='overflow-hidden w-full h-full rounded-xl relative border border-green-400/30'>
 			<div id={id} className='w-full h-full'></div>
-			<svg
-				className='absolute top-1/2 right-1/2 transform translate-x-1/2 -translate-y-1/2 opacity-10'
-				xmlns='http://www.w3.org/2000/svg'
-				width='70'
-				height='70'
-				viewBox='0 0 24 24'
-				stroke-width='1.5'
-				stroke='#597e8d'
-				fill='none'
-				stroke-linecap='round'
-				stroke-linejoin='round'
-			>
-				{(id === "html" && (
-					<>
-						<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-						<path d='M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z' />
-						<path d='M15.5 8h-7l.5 4h6l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5' />
-					</>
-				)) ||
-					(id === "css" && (
-						<>
-							<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-							<path d='M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z' />
-							<path d='M8.5 8h7l-4.5 4h4l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5' />
-						</>
-					)) ||
-					(id === "js" && (
-						<>
-							<path stroke='none' d='M0 0h24v24H0z' fill='none' />
-							<path d='M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z' />
-							<path d='M7.5 8h3v8l-2 -1' />
-							<path d='M16.5 8h-2.5a.5 .5 0 0 0 -.5 .5v3a.5 .5 0 0 0 .5 .5h1.423a.5 .5 0 0 1 .495 .57l-.418 2.93l-2 .5' />
-						</>
-					))}
-			</svg>
+			<EditorLogo language={id} />
 		</div>
 	)
 }
