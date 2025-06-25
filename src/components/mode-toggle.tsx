39fd8c100e32e6bc809dfa8a68ce2327d980473f
David Silgado Salcedo <dsilgadosalcedo@gmail.com>
2025-06-25 06:16:40 -0500

    Refactor ModeToggle component: remove unused theme state variable and add rounded-full class to SidebarMenuButton for improved styling.

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { Sun, Moon } from "lucide-react"
-import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -11,12 +10,12 @@ import useTheme from "@/hooks/useTheme"
 import { SidebarMenuButton } from "./ui/sidebar"
 
 export default function ModeToggle() {
-  const [theme, setTheme] = useTheme()
+  const [setTheme] = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <SidebarMenuButton tooltip={"Toggle theme"}>
+        <SidebarMenuButton tooltip={"Toggle theme"} className="rounded-full">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span>Toggle theme</span>
